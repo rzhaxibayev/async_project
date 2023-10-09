e55816010763e88165b0600ccf87615dfcbe97e7
Rauan Zhaxibayev <rzhaxibayev.o@wio.io>
2023-10-09 12:24:58 +0600

    Remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:async_project/common/app_router.dart';
 import 'package:async_project/di/di_resolver.dart';
 import 'package:async_project/features/home/cubit/home_cubit.dart';
 import 'package:async_project/features/home/home_screen.dart';
-import 'package:async_project/widgets/label.dart';
-import 'package:async_project/widgets/label2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -23,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Template app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -33,55 +31,6 @@ class MyApp extends StatelessWidget {
       home: BlocProvider(
         create: (_) => GetIt.instance.get<HomeCubit>(),
         child: const HomeScreen(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  StreamController<String> controller = StreamController<String>.broadcast();
-
-  @override
-  void dispose() {
-    controller.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Form(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter your email',
-                  ),
-                  onChanged: (value) => controller.add(value),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Label(stream: controller.stream),
-              Label2(stream: controller.stream),
-            ],
-          ),
-        ),
       ),
     );
   }
